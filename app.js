abb05e1b9628bfdf4f516ddebffbdaa0de445db5
Palkesh Soni <sonipalkesh43@gmail.com>
2024-01-15 21:19:54 +0530

    adding css and media query

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var search = document.getElementById("search")
-search.onclick=function (){
+search.onclick = function () {
     console.log("clicked")
     var input = document.getElementById("input").value
     console.log(input)
     var a = [];
-    if(input<2)
-    document.getElementById("demo").innerHTML = "No Prime Number Present";
-    else{
-        for(let i=2;i<=input;i++)
-        {
+    if (input < 2)
+        document.getElementById("demo").innerHTML = "No Prime Number Present";
+    else if (input == 2)
+        document.getElementById("demo").innerHTML = "Prime number till 2 is only 2";
+
+    else {
+        for (let i = 2; i <= input; i++) {
             let flag = 1;
-            for(let j=2;j<i;j++)
-            {
-                if(i%j==0)
-                {flag = 0;}
+            for (let j = 2; j < i; j++) {
+                if (i % j == 0) { flag = 0; }
             }
-            if(flag)
-            {
+            if (flag) {
                 a.push(i);
                 // console.log(a);
                 // console.log(i);
@@ -25,6 +24,8 @@ search.onclick=function (){
             // console.log(i);
         }
         console.log(a);
-        document.getElementById("demo").innerHTML = a;
+        document.getElementById("demo").innerHTML = `Prime Number till ${input} are <div id="array"> ${a} </div>`;
+        document.getElementById("array").innerHTML = JSON.stringify(a,null,3);
+        // document.getElementById("array").style.color = "red";
     }
 }
