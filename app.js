03e705cada8abb493f912b2da5db8b3b92418ae1
Palkesh Soni <sonipalkesh43@gmail.com>
2024-01-15 22:34:29 +0530

    add checking

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ search.onclick = function () {
     var input = document.getElementById("input").value
     console.log(input)
     var a = [];
-    if (input < 2)
+    if(input=="")
+    {
+        alert("Input a number")
+    }
+    else if (input < 2)
         document.getElementById("demo").innerHTML = "No Prime Number Present";
     else if (input == 2)
         document.getElementById("demo").innerHTML = "Prime number till 2 is only 2";
@@ -27,5 +31,32 @@ search.onclick = function () {
         document.getElementById("demo").innerHTML = `Prime Number till ${input} are <div id="array"> ${a} </div>`;
         document.getElementById("array").innerHTML = JSON.stringify(a,null,3);
         // document.getElementById("array").style.color = "red";
+    }
+}
+// var input1 = document.getElementById("input1").value
+search1.onclick = function () {
+    console.log("clicked")
+    var input1 = document.getElementById("input1").value
+    console.log(input1)
+    // var a = [];
+    if(input1=="")
+    {
+        alert("Input a number")
+    }
+    else if (input1 <= 2)
+        document.getElementById("demo").innerHTML = "Not a Prime Number";
+
+    else {
+            let flag = 1;
+            for (let j = 2; j*j < input1; j++) {
+                if(input1%j==0)
+                flag = 0;
+            }
+            if (flag) {
+                document.getElementById("demo").innerHTML=`${input1} is a Prime Number`;
+            }
+            else{
+                document.getElementById("demo").innerHTML=`${input1} is not a Prime Number`;
+            }
     }
 }
